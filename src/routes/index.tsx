73d5bb5352b19f8a2db8f1440a4820c732dcc6b1
koxx12-dev <80456351+koxx12-dev@users.noreply.github.com>
2025-06-21 14:19:56 +0200

    WIDEJOX

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,20 @@
-import { createFileRoute } from '@tanstack/solid-router';
+import { createFileRoute, useNavigate } from '@tanstack/solid-router';
+import najox from '../../public/assets/namjox.png';
 
 export const Route = createFileRoute('/')({
 	component: RouteComponent,
 });
 
 function RouteComponent() {
-	return <div>Hello "/"!</div>;
+	const navigate = useNavigate();
+
+	return (
+		<div class='h-screen w-full' on:click={() => {
+			navigate({
+				to: '/character'
+			})
+		}}>
+			<img src={najox} aria-hidden class='h-full w-full object-fill' />
+		</div>
+	);
 }
